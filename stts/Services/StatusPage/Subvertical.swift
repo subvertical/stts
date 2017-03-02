595da2fb053767a8cb4bbc8b93fd89c09384c47c
Huss Choroomi <hchoroomi@gmail.com>
2017-03-02 05:36:57 +0400

    Added Subvertical

---
+++ stts/Services/StatusPage/Subvertical.swift
@@ -0,0 +1,11 @@
+//
+//  Subvertical.swift
+//  stts
+//
+
+import Foundation
+
+class Subvertical: StatusPageService {
+    override var url: URL { return URL(string: "http://status.subvertical.com")! }
+    override var statusPageID: String { return "w7c57ksjt5qj" }
+}
